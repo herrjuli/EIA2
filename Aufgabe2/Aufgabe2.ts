5755f191296df6178e895eaf9137752d838f9ab3
Julink <Julink@DESKTOP-379DKJS>
2018-10-21 15:07:13 +0200

    Aufgabe2

---
+++ Aufgabe2/Aufgabe2.ts
@@ -0,0 +1,640 @@
+/*
+Aufgabe: 2 Uno
+Name: Julia Herr
+Matrikel: 259568
+Datum: 21.10.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+
+
+
+
+namespace Aufgabe2 {
+    //2a,11a,74a
+    interface Karte {
+        Farbe: string;
+        Wert: string;
+        Da: boolean;
+    }
+    let Karte1: Karte = {
+        Farbe: "red",
+        Wert: "0",
+        Da: true
+    }
+    let Karte2: Karte = {
+        Farbe: "red",
+        Wert: "1",
+        Da: true
+    }
+    let Karte2a: Karte = {
+        Farbe: "red",
+        Wert: "1",
+        Da: true
+    }
+    let Karte3: Karte = {
+        Farbe: "red",
+        Wert: "2",
+        Da: true
+    }
+    let Karte4: Karte = {
+        Farbe: "red",
+        Wert: "2",
+        Da: true
+    }
+    let Karte5: Karte = {
+        Farbe: "red",
+        Wert: "3",
+        Da: true
+    }
+    let Karte6: Karte = {
+        Farbe: "red",
+        Wert: "3",
+        Da: true
+    }
+    let Karte7: Karte = {
+        Farbe: "red",
+        Wert: "4",
+        Da: true
+    }
+    let Karte8: Karte = {
+        Farbe: "red",
+        Wert: "4",
+        Da: true
+    }
+    let Karte9: Karte = {
+        Farbe: "red",
+        Wert: "5",
+        Da: true
+    }
+    let Karte10: Karte = {
+        Farbe: "red",
+        Wert: "5",
+        Da: true
+    }
+    let Karte11: Karte = {
+        Farbe: "red",
+        Wert: "6",
+        Da: true
+    }
+    let Karte11a: Karte = {
+        Farbe: "red",
+        Wert: "6",
+        Da: true
+    }
+    let Karte12: Karte = {
+        Farbe: "red",
+        Wert: "7",
+        Da: true
+    }
+    let Karte13: Karte = {
+        Farbe: "red",
+        Wert: "7",
+        Da: true
+    }
+    let Karte14: Karte = {
+        Farbe: "red",
+        Wert: "8",
+        Da: true
+    }
+    let Karte15: Karte = {
+        Farbe: "red",
+        Wert: "8",
+        Da: true
+    }
+    let Karte16: Karte = {
+        Farbe: "red",
+        Wert: "9",
+        Da: true
+    }
+    let Karte17: Karte = {
+        Farbe: "red",
+        Wert: "9",
+        Da: true
+    }
+    let Karte18: Karte = {
+        Farbe: "red",
+        Wert: "Aussetzen",
+        Da: true
+    }
+    let Karte19: Karte = {
+        Farbe: "red",
+        Wert: "Aussetzen",
+        Da: true
+    }
+    let Karte20: Karte = {
+        Farbe: "red",
+        Wert: "Richtungswechsel",
+        Da: true
+    }
+    let Karte21: Karte = {
+        Farbe: "red",
+        Wert: "Richtungswechsel",
+        Da: true
+    }
+    let Karte22: Karte = {
+        Farbe: "red",
+        Wert: "+2",
+        Da: true
+    }
+    let Karte23: Karte = {
+        Farbe: "red",
+        Wert: "+2",
+        Da: true
+    }
+    let Karte24: Karte = {
+        Farbe: "blue",
+        Wert: "0",
+        Da: true
+    }
+
+    let Karte25: Karte = {
+        Farbe: "blue",
+        Wert: "1",
+        Da: true
+    }
+    let Karte26: Karte = {
+        Farbe: "blue",
+        Wert: "1",
+        Da: true
+    }
+    let Karte27: Karte = {
+        Farbe: "blue",
+        Wert: "2",
+        Da: true
+    }
+    let Karte28: Karte = {
+        Farbe: "blue",
+        Wert: "2",
+        Da: true
+    }
+    let Karte29: Karte = {
+        Farbe: "blue",
+        Wert: "3",
+        Da: true
+    }
+    let Karte30: Karte = {
+        Farbe: "blue",
+        Wert: "3",
+        Da: true
+    }
+    let Karte31: Karte = {
+        Farbe: "blue",
+        Wert: "4",
+        Da: true
+    }
+    let Karte32: Karte = {
+        Farbe: "blue",
+        Wert: "4",
+        Da: true
+    }
+    let Karte33: Karte = {
+        Farbe: "blue",
+        Wert: "5",
+        Da: true
+    }
+    let Karte34: Karte = {
+        Farbe: "blue",
+        Wert: "5",
+        Da: true
+    }
+    let Karte35: Karte = {
+        Farbe: "blue",
+        Wert: "6",
+        Da: true
+    }
+    let Karte36: Karte = {
+        Farbe: "blue",
+        Wert: "6",
+        Da: true
+    }
+    let Karte37: Karte = {
+        Farbe: "blue",
+        Wert: "7",
+        Da: true
+    }
+    let Karte38: Karte = {
+        Farbe: "blue",
+        Wert: "7",
+        Da: true
+    }
+    let Karte39: Karte = {
+        Farbe: "blue",
+        Wert: "8",
+        Da: true
+    }
+    let Karte40: Karte = {
+        Farbe: "blue",
+        Wert: "8",
+        Da: true
+    }
+    let Karte41: Karte = {
+        Farbe: "blue",
+        Wert: "9",
+        Da: true
+    }
+    let Karte42: Karte = {
+        Farbe: "blue",
+        Wert: "9",
+        Da: true
+    }
+    let Karte43: Karte = {
+        Farbe: "blue",
+        Wert: "Aussetzen",
+        Da: true
+    }
+    let Karte44: Karte = {
+        Farbe: "blue",
+        Wert: "Aussetzen",
+        Da: true
+    }
+    let Karte45: Karte = {
+        Farbe: "blue",
+        Wert: "Richtungswechsel",
+        Da: true
+    }
+    let Karte46: Karte = {
+        Farbe: "blue",
+        Wert: "Richtungswechsel",
+        Da: true
+    }
+    let Karte47: Karte = {
+        Farbe: "blue",
+        Wert: "+2",
+        Da: true
+    }
+    let Karte48: Karte = {
+        Farbe: "blue",
+        Wert: "+2",
+        Da: true
+    }
+    let Karte49: Karte = {
+        Farbe: "green",
+        Wert: "0",
+        Da: true
+    }
+
+    let Karte50: Karte = {
+        Farbe: "green",
+        Wert: "1",
+        Da: true
+    }
+    let Karte51: Karte = {
+        Farbe: "green",
+        Wert: "1",
+        Da: true
+    }
+    let Karte52: Karte = {
+        Farbe: "green",
+        Wert: "2",
+        Da: true
+    }
+    let Karte53: Karte = {
+        Farbe: "green",
+        Wert: "2",
+        Da: true
+    }
+    let Karte54: Karte = {
+        Farbe: "green",
+        Wert: "3",
+        Da: true
+    }
+    let Karte55: Karte = {
+        Farbe: "green",
+        Wert: "3",
+        Da: true
+    }
+    let Karte56: Karte = {
+        Farbe: "green",
+        Wert: "4",
+        Da: true
+    }
+    let Karte57: Karte = {
+        Farbe: "green",
+        Wert: "4",
+        Da: true
+    }
+    let Karte58: Karte = {
+        Farbe: "green",
+        Wert: "5",
+        Da: true
+    }
+    let Karte59: Karte = {
+        Farbe: "green",
+        Wert: "5",
+        Da: true
+    }
+    let Karte60: Karte = {
+        Farbe: "green",
+        Wert: "6",
+        Da: true
+    }
+    let Karte61: Karte = {
+        Farbe: "green",
+        Wert: "6",
+        Da: true
+    }
+    let Karte62: Karte = {
+        Farbe: "green",
+        Wert: "7",
+        Da: true
+    }
+    let Karte63: Karte = {
+        Farbe: "green",
+        Wert: "7",
+        Da: true
+    }
+    let Karte64: Karte = {
+        Farbe: "green",
+        Wert: "8",
+        Da: true
+    }
+    let Karte65: Karte = {
+        Farbe: "green",
+        Wert: "8",
+        Da: true
+    }
+    let Karte66: Karte = {
+        Farbe: "green",
+        Wert: "9",
+        Da: true
+    }
+    let Karte67: Karte = {
+        Farbe: "green",
+        Wert: "9",
+        Da: true
+    }
+    let Karte68: Karte = {
+        Farbe: "green",
+        Wert: "Aussetzen",
+        Da: true
+    }
+    let Karte69: Karte = {
+        Farbe: "green",
+        Wert: "Aussetzen",
+        Da: true
+    }
+    let Karte70: Karte = {
+        Farbe: "green",
+        Wert: "Richtungswechsel",
+        Da: true
+    }
+    let Karte71: Karte = {
+        Farbe: "green",
+        Wert: "Richtungswechsel",
+        Da: true
+    }
+    let Karte72: Karte = {
+        Farbe: "green",
+        Wert: "+2",
+        Da: true
+    }
+    let Karte73: Karte = {
+        Farbe: "green",
+        Wert: "+2",
+        Da: true
+    }
+    let Karte74: Karte = {
+        Farbe: "yellow",
+        Wert: "0",
+        Da: true
+    }
+
+    let Karte74a: Karte = {
+        Farbe: "yellow",
+        Wert: "1",
+        Da: true
+    }
+    let Karte75: Karte = {
+        Farbe: "yellow",
+        Wert: "1",
+        Da: true
+    }
+    let Karte76: Karte = {
+        Farbe: "yellow",
+        Wert: "2",
+        Da: true
+    }
+    let Karte77: Karte = {
+        Farbe: "yellow",
+        Wert: "2",
+        Da: true
+    }
+    let Karte78: Karte = {
+        Farbe: "yellow",
+        Wert: "3",
+        Da: true
+    }
+    let Karte79: Karte = {
+        Farbe: "yellow",
+        Wert: "3",
+        Da: true
+    }
+    let Karte80: Karte = {
+        Farbe: "yellow",
+        Wert: "4",
+        Da: true
+    }
+    let Karte81: Karte = {
+        Farbe: "yellow",
+        Wert: "4",
+        Da: true
+    }
+    let Karte82: Karte = {
+        Farbe: "yellow",
+        Wert: "5",
+        Da: true
+    }
+    let Karte83: Karte = {
+        Farbe: "yellow",
+        Wert: "5",
+        Da: true
+    }
+    let Karte84: Karte = {
+        Farbe: "yellow",
+        Wert: "6",
+        Da: true
+    }
+    let Karte85: Karte = {
+        Farbe: "yellow",
+        Wert: "6",
+        Da: true
+    }
+    let Karte86: Karte = {
+        Farbe: "yellow",
+        Wert: "7",
+        Da: true
+    }
+    let Karte87: Karte = {
+        Farbe: "yellow",
+        Wert: "7",
+        Da: true
+    }
+    let Karte88: Karte = {
+        Farbe: "yellow",
+        Wert: "8",
+        Da: true
+    }
+    let Karte89: Karte = {
+        Farbe: "yellow",
+        Wert: "8",
+        Da: true
+    }
+    let Karte90: Karte = {
+        Farbe: "yellow",
+        Wert: "9",
+        Da: true
+    }
+    let Karte91: Karte = {
+        Farbe: "yellow",
+        Wert: "9",
+        Da: true
+    }
+    let Karte92: Karte = {
+        Farbe: "yellow",
+        Wert: "Aussetzen",
+        Da: true
+    }
+    let Karte93: Karte = {
+        Farbe: "yellow",
+        Wert: "Aussetzen",
+        Da: true
+    }
+    let Karte94: Karte = {
+        Farbe: "yellow",
+        Wert: "Richtungswechsel",
+        Da: true
+    }
+    let Karte95: Karte = {
+        Farbe: "yellow",
+        Wert: "Richtungswechsel",
+        Da: true
+    }
+    let Karte96: Karte = {
+        Farbe: "yellow",
+        Wert: "+2",
+        Da: true
+    }
+    let Karte97: Karte = {
+        Farbe: "yellow",
+        Wert: "+2",
+        Da: true
+    }
+    let Karte98: Karte = {
+        Farbe: "black",
+        Wert: "+4",
+        Da: true
+    }
+
+    let Karte99: Karte = {
+        Farbe: "black",
+        Wert: "+4",
+        Da: true
+    }
+    let Karte100: Karte = {
+        Farbe: "black",
+        Wert: "+4",
+        Da: true
+    }
+    let Karte101: Karte = {
+        Farbe: "black",
+        Wert: "+4",
+        Da: true
+    }
+    let Karte102: Karte = {
+        Farbe: "black",
+        Wert: "Farbwahl",
+        Da: true
+    }
+    let Karte103: Karte = {
+        Farbe: "black",
+        Wert: "Farbwahl",
+        Da: true
+    }
+    let Karte104: Karte = {
+        Farbe: "black",
+        Wert: "Farbwahl",
+        Da: true
+    }
+    let Karte105: Karte = {
+        Farbe: "black",
+        Wert: "Farbwahl",
+        Da: true
+    }
+    let Deck: Karte[] = [Karte1, Karte2, Karte2a, Karte3, Karte4, Karte5, Karte6, Karte7, Karte8, Karte9, Karte10, Karte11, Karte11a, Karte12, Karte13, Karte14, Karte15, Karte16, Karte17, Karte18, Karte19, Karte20, Karte21, Karte22, Karte23, Karte24, Karte25, Karte26, Karte27, Karte28, Karte29, Karte30, Karte31, Karte32, Karte33, Karte34, Karte35, Karte36, Karte37, Karte38, Karte39, Karte40, Karte41, Karte42, Karte43, Karte44, Karte45, Karte46, Karte47, Karte48, Karte49, Karte50, Karte51, Karte52, Karte53, Karte54, Karte55, Karte56, Karte57, Karte58, Karte59, Karte60, Karte61, Karte62, Karte63, Karte64, Karte65, Karte66, Karte67, Karte68, Karte69, Karte70, Karte71, Karte72, Karte73, Karte74, Karte74a, Karte75, Karte76, Karte77, Karte78, Karte79, Karte80, Karte81, Karte82, Karte83, Karte84, Karte85, Karte86, Karte87, Karte88, Karte89, Karte90, Karte91, Karte92, Karte93, Karte94, Karte95, Karte96, Karte97, Karte98, Karte99, Karte100, Karte101, Karte102, Karte103, Karte104, Karte105]
+
+    function Auswahl() {
+        let d: string = prompt();
+        let o: number;
+        o = Number(d);
+        for (let i: number = 0; i < o; i++) {
+            let x: number = Math.floor(Math.random() * Math.floor(105));
+            if (Deck[x].Da == false) {
+                i--;
+                continue;
+            } else if (Deck[x].Da == true) {
+                Deck[x].Da = false;
+                start(Deck[x].Farbe, Deck[x].Wert, i)
+            }
+        }
+    }
+    function start(_color: string, _nummer: string, n: number) {
+        let ab = n * 150;
+        let div : HTMLDivElement= document.createElement("div");
+        div.setAttribute("id", "Karte" + n);
+        document.body.appendChild(div);
+        document.getElementById("Karte" + n).innerHTML += _nummer;
+        let s = div.style;
+        s.border = "thin solid black";
+        s.position = "absolute";
+        s.backgroundColor = _color;
+        s.color = "white";
+        s.width = 135 + "px";
+        s.height = 200 + "px";
+        s.left = ab + "px";
+        s.bottom = 25 + "px";
+        s.textAlign = "center";
+        s.fontSize = 2 + "em";
+        s.borderRadius = 10 + "px";
+      
+    }
+    function Stapel() {
+        let Ablage = document.createElement("div");
+        document.body.appendChild(Ablage);
+        Ablage.setAttribute("id", "ablage");
+        document.getElementById("ablage").innerHTML += "Ablage";
+        let a = Ablage.style;
+        a.border = "thin solid black";
+        a.position = "absolute";
+        a.backgroundColor = "black";
+        a.width = 128 + "px";
+        a.height = 200 + "px";
+        a.right = 1025 + "px";
+        a.top = 25 + "px";
+        a.textAlign = "center";
+        a.borderRadius = 10 + "px";
+        a.color = "yellow";
+         a.fontSize = 2 + "em";
+        let Aufnahme = document.createElement("div");
+        document.body.appendChild(Aufnahme);
+        Aufnahme.setAttribute("id", "aufnahme");
+        document.getElementById("aufnahme").innerHTML += "Aufnahme";
+        let z = Aufnahme.style;
+        z.border = "thin solid black";
+        z.position = "absolute";
+        z.backgroundColor = "black";
+        z.width = 128 + "px";
+        z.height = 200 + "px";
+        z.left = 25 + "px";
+        z.top = 25 + "px";
+        z.textAlign = "center";
+        z.borderRadius = 10 + "px";
+        z.color = "yellow";
+         z.fontSize = 2 + "em";
+    }
+    Stapel();
+
+    document.addEventListener('DOMContentLoaded', Auswahl);
+
+} 
